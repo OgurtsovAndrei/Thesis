0c6fd2cb9896605b277776338089d44adbbdb30d
Andrei Ogurtsov <andrei.ogurtsov@point-devel.com>
2026-01-25 16:44:25 +0200

    rloc: Local Exact Range Locator (lerloc) v1

---
+++ rloc/lerloc.go
@@ -0,0 +1,43 @@
+package rloc
+
+import (
+	"Thesis/bits"
+	"Thesis/zfasttrie"
+)
+
+type LocalExactRangeLocator struct {
+	hzft *zfasttrie.HZFastTrie[uint32]
+	rl   *RangeLocator
+}
+
+func NewLocalExactRangeLocator(keys []bits.BitString) *LocalExactRangeLocator {
+	if len(keys) == 0 {
+		return &LocalExactRangeLocator{}
+	}
+
+	zt := zfasttrie.Build(keys)
+	hzft := zfasttrie.NewHZFastTrie[uint32](keys)
+	rl := NewRangeLocator(zt)
+
+	return &LocalExactRangeLocator{
+		hzft: hzft,
+		rl:   rl,
+	}
+}
+
+func (lerl *LocalExactRangeLocator) WeakPrefixSearch(prefix bits.BitString) (int, int, error) {
+	if lerl.hzft == nil || lerl.rl == nil {
+		return 0, 0, nil
+	}
+
+	exitNodeLength := lerl.hzft.GetExistingPrefix(prefix)
+
+	var exitNode bits.BitString
+	if exitNodeLength == 0 {
+		exitNode = bits.NewBitString("")
+	} else {
+		exitNode = prefix.Prefix(int(exitNodeLength))
+	}
+
+	return lerl.rl.Query(exitNode)
+}
