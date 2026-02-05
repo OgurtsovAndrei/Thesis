583e4870d4efef850b8670c9df174cecf3d978cc
Andrei Ogurtsov <andrei.ogurtsov@jetbrains.com>
2026-02-05 17:16:56 +0200

    rloc: extend benchmarks to support multiple bit lengths per key

--- rloc/test_utils.go
+++ rloc/test_utils.go
@@ -8,37 +8,41 @@ import (
 )
 
 var (
-	benchKeyCounts = []int{1 << 5, 1 << 8, 1 << 10, 1 << 13, 1 << 15, 1 << 18}
-	benchBitLength = 64 // Fixed bit length for all keys
-	benchKeys      map[int][]bits.BitString
-	benchOnce      sync.Once
+	benchKeyCounts  = []int{1 << 5, 1 << 8, 1 << 10, 1 << 13, 1 << 15, 1 << 18}
+	benchBitLengths = []int{64, 128, 256, 512, 1024}
+	benchKeys       map[int]map[int][]bits.BitString // [bitLength][keyCount]
+	benchOnce       sync.Once
 )
 
 func initBenchKeys() {
 	benchOnce.Do(func() {
-		benchKeys = make(map[int][]bits.BitString)
-		for _, count := range benchKeyCounts {
-			rawKeys := buildUniqueStrKeys(count)
+		benchKeys = make(map[int]map[int][]bits.BitString)
+		for _, bitLen := range benchBitLengths {
+			benchKeys[bitLen] = make(map[int][]bits.BitString)
+			for _, count := range benchKeyCounts {
+				rawKeys := buildUniqueStrKeys(count, bitLen)
 
-			bsKeys := make([]bits.BitString, count)
-			for i, k := range rawKeys {
-				bsKeys[i] = bits.NewFromText(k)
+				bsKeys := make([]bits.BitString, count)
+				for i, k := range rawKeys {
+					bsKeys[i] = bits.NewFromText(k)
+				}
+
+				sort.Sort(bitStringSorter(bsKeys))
+
+				benchKeys[bitLen][count] = bsKeys
 			}
-
-			sort.Sort(bitStringSorter(bsKeys))
-
-			benchKeys[count] = bsKeys
 		}
 	})
 }
 
-func buildUniqueStrKeys(size int) []string {
+func buildUniqueStrKeys(size int, bitLength int) []string {
 	keys := make([]string, size)
 	unique := make(map[string]bool, size)
+	byteLength := (bitLength + 7) / 8 // Round up to nearest byte
 
 	for i := 0; i < size; i++ {
 		for {
-			b := make([]byte, 8)
+			b := make([]byte, byteLength)
 			_, _ = rand.Read(b)
 			s := string(b)
 			if !unique[s] {
