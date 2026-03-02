d522de419b920483512f65e0e24f91fb3973433f
AndreiOgurtsov <ogurcovwm@gmail.com>
2026-03-02 18:49:20 +0400

    docs(trie): restructure benchmarks and stabilize SHZFT research

--- trie/shzft/bench_test.go
+++ trie/shzft/bench_test.go
@@ -1,34 +1,49 @@
 package shzft
 
 import (
+	"Thesis/locators/rloc"
 	"Thesis/trie/hzft"
 	"fmt"
+	"math/rand"
 	"testing"
 )
 
-var benchKeyCounts = []int{1024, 8192, 32768, 131072}
-
 func BenchmarkMemory(b *testing.B) {
-	for _, l := range []int{64, 256, 1024} {
-		for _, n := range benchKeyCounts {
-			b.Run(fmt.Sprintf("HZFT_L=%d_N=%d", l, n), func(b *testing.B) {
-				keys := genRandomKeys(n, l, 42)
+	rloc.InitBenchKeys()
+
+	// Production-realistic key counts (matching rloc.BenchKeyCounts)
+	benchCounts := []int{1024, 8192, 32768, 262144}
+
+	for _, l := range rloc.BenchBitLengths {
+		for _, n := range benchCounts {
+			// Skip extreme combinations to save time
+			if n > 65536 && l > 1024 {
+				continue
+			}
+
+			b.Run(fmt.Sprintf("HZFT/L=%d/N=%d", l, n), func(b *testing.B) {
+				keys := rloc.BenchKeys[l][n]
 				b.ReportAllocs()
 				b.ResetTimer()
 				for i := 0; i < b.N; i++ {
-					trie := hzft.NewHZFastTrie[uint16](keys)
+					trie := hzft.NewHZFastTrie[uint32](keys)
+					if i == 0 {
+						b.Logf("JSON_MEM_REPORT: %s", trie.MemDetailed().JSON())
+					}
 					b.ReportMetric(float64(trie.ByteSize())*8.0/float64(n), "bits/key")
 				}
 			})
 
-			b.Run(fmt.Sprintf("SHZFT_L=%d_N=%d", l, n), func(b *testing.B) {
-				keys := genRandomKeys(n, l, 42)
+			b.Run(fmt.Sprintf("SHZFT/L=%d/N=%d", l, n), func(b *testing.B) {
+				keys := rloc.BenchKeys[l][n]
 				b.ReportAllocs()
 				b.ResetTimer()
 				for i := 0; i < b.N; i++ {
 					trie := NewSuccinctHZFastTrie(keys)
+					if i == 0 {
+						b.Logf("JSON_MEM_REPORT: %s", trie.MemDetailed().JSON())
+					}
 					b.ReportMetric(float64(trie.ByteSize())*8.0/float64(n), "bits/key")
-					b.ReportMetric(float64(trie.deltaBits), "delta_bits")
 				}
 			})
 		}
@@ -36,25 +51,41 @@ func BenchmarkMemory(b *testing.B) {
 }
 
 func BenchmarkQuery(b *testing.B) {
-	for _, l := range []int{64, 1024} {
-		for _, n := range []int{32768} {
-			keys := genRandomKeys(n, l, 42)
-			hzftTrie := hzft.NewHZFastTrie[uint16](keys)
+	rloc.InitBenchKeys()
+	benchCounts := []int{1024, 8192, 32768, 262144}
+
+	for _, l := range []int{64, 256, 1024, 4096} {
+		for _, n := range benchCounts {
+			if n > 65536 && l > 1024 {
+				continue
+			}
+			keys := rloc.BenchKeys[l][n]
+			hzftTrie := hzft.NewHZFastTrie[uint32](keys)
 			shzftTrie := NewSuccinctHZFastTrie(keys)
 
-			b.Run(fmt.Sprintf("HZFT_Query_L=%d_N=%d", l, n), func(b *testing.B) {
+			// Shuffle query indices to avoid sequential access patterns
+			queryIndices := make([]int, n)
+			for i := range queryIndices {
+				queryIndices[i] = i
+			}
+			r := rand.New(rand.NewSource(42))
+			r.Shuffle(len(queryIndices), func(i, j int) {
+				queryIndices[i], queryIndices[j] = queryIndices[j], queryIndices[i]
+			})
+
+			b.Run(fmt.Sprintf("HZFT/Query/L=%d/N=%d", l, n), func(b *testing.B) {
 				b.ReportAllocs()
 				b.ResetTimer()
 				for i := 0; i < b.N; i++ {
-					_ = hzftTrie.GetExistingPrefix(keys[i%n])
+					_ = hzftTrie.GetExistingPrefix(keys[queryIndices[i%n]])
 				}
 			})
 
-			b.Run(fmt.Sprintf("SHZFT_Query_L=%d_N=%d", l, n), func(b *testing.B) {
+			b.Run(fmt.Sprintf("SHZFT/Query/L=%d/N=%d", l, n), func(b *testing.B) {
 				b.ReportAllocs()
 				b.ResetTimer()
 				for i := 0; i < b.N; i++ {
-					_ = shzftTrie.GetExistingPrefix(keys[i%n])
+					_ = shzftTrie.GetExistingPrefix(keys[queryIndices[i%n]])
 				}
 			})
 		}
