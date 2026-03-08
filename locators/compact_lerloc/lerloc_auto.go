8f105610b3165ef36e83e47869fb7ad5c3a66bed
Andrei Ogurtsov <andrei.ogurtsov@jetbrains.com>
2026-03-08 19:01:12 +0200

    feat(locators): replace HZFastTrie with SuccinctHZFastTrie in Compact LERLOC to further reduce memory

--- locators/compact_lerloc/lerloc_auto.go
+++ locators/compact_lerloc/lerloc_auto.go
@@ -4,31 +4,12 @@ import (
 	"Thesis/bits"
 )
 
-// NewAutoCompactLocalExactRangeLocator analyzes the maximum key length and chooses
-// the smallest uint type (E) that can accommodate it to minimize the memory
-// overhead of the HZFastTrie component.
+// NewAutoCompactLocalExactRangeLocator returns a CompactLocalExactRangeLocator.
+// It is kept for backwards compatibility with test and benchmark files that
+// expected the generic auto-selection behavior.
 func NewAutoCompactLocalExactRangeLocator(keys []bits.BitString) (interface {
 	WeakPrefixSearch(prefix bits.BitString) (int, int, error)
 	ByteSize() int
 }, error) {
-	if len(keys) == 0 {
-		return NewCompactLocalExactRangeLocator[uint8](keys)
-	}
-
-	maxLen := 0
-	for _, key := range keys {
-		if l := int(key.Size()); l > maxLen {
-			maxLen = l
-		}
-	}
-
-	if maxLen <= 255 {
-		return NewCompactLocalExactRangeLocator[uint8](keys)
-	} else if maxLen <= 65535 {
-		return NewCompactLocalExactRangeLocator[uint16](keys)
-	} else if maxLen <= 4294967295 {
-		return NewCompactLocalExactRangeLocator[uint32](keys)
-	} else {
-		return NewCompactLocalExactRangeLocator[uint64](keys)
-	}
+	return NewCompactLocalExactRangeLocator(keys)
 }
