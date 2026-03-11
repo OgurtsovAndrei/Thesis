6f50e5cb5c85d185509194494b81605bdbb41615
Andrei Ogurtsov <andrei.ogurtsov@jetbrains.com>
2026-03-11 20:33:11 +0200

    fix(local_exact_range): handle zero bitWidth in `packUint64Local` to avoid nil slice creation

--- local_exact_range/exact_range_emptiness.go
+++ local_exact_range/exact_range_emptiness.go
@@ -15,12 +15,12 @@ type ExactRangeEmptiness struct {
 	D1         *rsdic.RSDic
 	D2         *rsdic.RSDic
 	packedData []uint64
-	
-	n          int
-	numBlocks  int
-	L          uint32
-	k          uint32
-	w          uint32
+
+	n         int
+	numBlocks int
+	L         uint32
+	k         uint32
+	w         uint32
 }
 
 func NewExactRangeEmptiness(keys []bits.BitString, universe bits.BitString) (*ExactRangeEmptiness, error) {
@@ -36,17 +36,21 @@ func NewExactRangeEmptiness(keys []bits.BitString, universe bits.BitString) (*Ex
 	}
 
 	k := uint32(math.Floor(math.Log2(float64(n))))
-	if k == 0 { k = 1 }
-	
+	if k == 0 {
+		k = 1
+	}
+
 	numBlocks := 1 << k
 	L := universe.Size()
-	if L < k { L = k }
+	if L < k {
+		L = k
+	}
 	w := L - k
 
 	D1 := rsdic.New()
 	D2 := rsdic.New()
 	suffixes := make([]uint64, 0, n)
-	
+
 	i := 0
 	for b := 0; b < numBlocks; b++ {
 		countInBlock := 0
@@ -55,7 +59,7 @@ func NewExactRangeEmptiness(keys []bits.BitString, universe bits.BitString) (*Ex
 			countInBlock++
 			i++
 		}
-		
+
 		if countInBlock > 0 {
 			D1.PushBack(true)
 			D2.PushBack(true)
@@ -109,15 +113,23 @@ func extractSuffixAsUint64(bs bits.BitString, L, w uint32) uint64 {
 }
 
 func (ere *ExactRangeEmptiness) IsEmpty(a, b bits.BitString) bool {
-	if ere.n == 0 { return true }
-	if a.Compare(b) > 0 { return true }
+	if ere.n == 0 {
+		return true
+	}
+	if a.Compare(b) > 0 {
+		return true
+	}
 
 	blockA := getBlockIndex(a, ere.k)
 	blockB := getBlockIndex(b, ere.k)
 
 	// Range exceeds universe
-	if blockA >= uint64(ere.numBlocks) { return true }
-	if blockB >= uint64(ere.numBlocks) { blockB = uint64(ere.numBlocks - 1) }
+	if blockA >= uint64(ere.numBlocks) {
+		return true
+	}
+	if blockB >= uint64(ere.numBlocks) {
+		blockB = uint64(ere.numBlocks - 1)
+	}
 
 	// 1. Check intermediate full blocks
 	if blockB > blockA+1 {
@@ -144,7 +156,9 @@ func (ere *ExactRangeEmptiness) IsEmpty(a, b bits.BitString) bool {
 			start, end := ere.getBlockRange(blockA)
 			suffA := extractSuffixAsUint64(a, ere.L, ere.w)
 			maxSuff := (uint64(1) << ere.w) - 1
-			if ere.w == 64 { maxSuff = ^uint64(0) }
+			if ere.w == 64 {
+				maxSuff = ^uint64(0)
+			}
 			if !ere.isRangeEmptyInBlock(start, end, suffA, maxSuff) {
 				return false
 			}
@@ -167,7 +181,7 @@ func (ere *ExactRangeEmptiness) getBlockRange(blockIdx uint64) (int, int) {
 	posInD2 := ere.D2.Select(uint64(numNonEmptyBefore), true)
 	startIndex := int(posInD2 - uint64(numNonEmptyBefore))
 	posEndInD2 := ere.D2.Select(uint64(numNonEmptyBefore+1), true)
-	endIndex := int(posEndInD2 - uint64(numNonEmptyBefore + 1))
+	endIndex := int(posEndInD2 - uint64(numNonEmptyBefore+1))
 	return startIndex, endIndex
 }
 
@@ -182,7 +196,7 @@ func (ere *ExactRangeEmptiness) isRangeEmptyInBlock(start, end int, minSuff, max
 			r = mid
 		}
 	}
-	
+
 	if l < end {
 		val := ere.getPackedSuffix(l)
 		if val <= maxSuff {
@@ -197,7 +211,9 @@ func (ere *ExactRangeEmptiness) getPackedSuffix(idx int) uint64 {
 }
 
 func packUint64Local(values []uint64, bitWidth int) []uint64 {
-	if len(values) == 0 { return nil }
+	if len(values) == 0 || bitWidth == 0 {
+		return nil
+	}
 	totalBits := uint64(len(values)) * uint64(bitWidth)
 	numWords := (totalBits + 63) / 64
 	packed := make([]uint64, numWords)
@@ -214,7 +230,9 @@ func packUint64Local(values []uint64, bitWidth int) []uint64 {
 }
 
 func (ere *ExactRangeEmptiness) ByteSize() int {
-	if ere == nil || ere.n == 0 { return 0 }
+	if ere == nil || ere.n == 0 {
+		return 0
+	}
 	size := int(unsafe.Sizeof(*ere))
 	size += ere.D1.AllocSize()
 	size += ere.D2.AllocSize()
@@ -223,13 +241,13 @@ func (ere *ExactRangeEmptiness) ByteSize() int {
 }
 
 type Stats struct {
-	N                int
-	NumBlocks        int
-	NonEmptyBlocks   int
-	EmptyBlocks      int
-	AvgKeysPerBlock  float64
-	MaxKeysInBlock   int
-	EmptyBlockPct    float64
+	N               int
+	NumBlocks       int
+	NonEmptyBlocks  int
+	EmptyBlocks     int
+	AvgKeysPerBlock float64
+	MaxKeysInBlock  int
+	EmptyBlockPct   float64
 }
 
 func (ere *ExactRangeEmptiness) GetStats() Stats {
@@ -257,9 +275,11 @@ func (ere *ExactRangeEmptiness) GetStats() Stats {
 }
 
 func (ere *ExactRangeEmptiness) MemDetailed() utils.MemReport {
-	if ere == nil || ere.n == 0 { return utils.MemReport{Name: "ExactRangeEmptiness", TotalBytes: 0} }
+	if ere == nil || ere.n == 0 {
+		return utils.MemReport{Name: "ExactRangeEmptiness", TotalBytes: 0}
+	}
 	return utils.MemReport{
-		Name: "ExactRangeEmptiness",
+		Name:       "ExactRangeEmptiness",
 		TotalBytes: ere.ByteSize(),
 		Children: []utils.MemReport{
 			{Name: "metadata", TotalBytes: int(unsafe.Sizeof(*ere))},
