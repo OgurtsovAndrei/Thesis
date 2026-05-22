b0dc64f6c8cabd8aadd2b5641d45f655cc5d68d0
Andrei Ogurtsov <andrei.ogurtsov@jetbrains.com>
2026-05-22 11:23:55 +0300

    chore(eval): fix ahsh usage

--- emptiness/run_eval/soda_exact/run_eval.go
+++ emptiness/run_eval/soda_exact/run_eval.go
@@ -3,40 +3,46 @@ package main
 import (
 	"Thesis/emptiness/exact/ere"
 	ere_one_d "Thesis/emptiness/exact/ere_one_d"
-    "Thesis/emptiness/internal/hash"
+	"Thesis/emptiness/internal/hash"
 	"fmt"
 	"math/rand"
-    "sort"
+	"sort"
 )
 
 func main() {
 	n := 1 << 20
 	keys := make([]uint64, n)
-	step := (uint64(1) << 64) / uint64(n)
+	step := uint64(1) << (64 - 20)
 	for i := 0; i < n; i++ {
 		keys[i] = uint64(i)*step + (rand.Uint64() % step)
 	}
 
-    // SODA hash them
-    K := uint32(34)
-    h, _ := hash.NewLocalityPreservingHash(64, K, n)
-    
-    hashedKeys := make([]uint64, n)
-    for i := 0; i < n; i++ {
-        hashedKeys[i] = h.Hash(keys[i])
-    }
-    
-    // SODA ERE requires sorted hashed keys
-    sort.Slice(hashedKeys, func(i, j int) bool { return hashedKeys[i] < hashedKeys[j] })
-    // Deduplicate
-    unique := make([]uint64, 0, n)
-    unique = append(unique, hashedKeys[0])
-    for i := 1; i < n; i++ {
-        if hashedKeys[i] != hashedKeys[i-1] {
-            unique = append(unique, hashedKeys[i])
-        }
-    }
-    fmt.Printf("Unique keys: %d / %d\n", len(unique), n)
+	// SODA hash them
+	K := uint32(34)
+	seed := int64(128)
+	rng := rand.New(rand.NewSource(seed))
+	hashA := rng.Uint64() | 1
+	hashB := rng.Uint64()
+	rMask := (uint64(1) << K) - 1
+
+	hashedKeys := make([]uint64, n)
+	for i := 0; i < n; i++ {
+		blockIdx := keys[i] >> K
+		ux := hash.PairwiseHash(blockIdx, hashA, hashB, K)
+		hashedKeys[i] = (ux + keys[i]) & rMask
+	}
+
+	// SODA ERE requires sorted hashed keys
+	sort.Slice(hashedKeys, func(i, j int) bool { return hashedKeys[i] < hashedKeys[j] })
+	// Deduplicate
+	unique := make([]uint64, 0, n)
+	unique = append(unique, hashedKeys[0])
+	for i := 1; i < n; i++ {
+		if hashedKeys[i] != hashedKeys[i-1] {
+			unique = append(unique, hashedKeys[i])
+		}
+	}
+	fmt.Printf("Unique keys: %d / %d\n", len(unique), n)
 
 	e1, _ := ere.NewExactRangeEmptiness(unique, K)
 	e2, _ := ere_one_d.NewExactRangeEmptiness(unique, K)
